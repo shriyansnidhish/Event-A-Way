b5aa95c8ce0ea650a86491e04e2e1df850f9c06f
Shriyans Nidhish <nidishsandwar493@gmail.com>
2023-09-15 21:29:23 -0400

    Update views.py

--- chatgpt/views.py
+++ chatgpt/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import openai
 
-OPENAI_TOKEN = "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"
+OPENAI_TOKEN = "xxxx"
 openai.api_key = OPENAI_TOKEN
 # Create your views here.
 def ask_question(request):
